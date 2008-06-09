5db85fbb1378d9352c2980f8b401e708c1f78a5a
trent.p.nelson <trent.p.nelson@f595771e-6544-0410-99e0-6d4d7ded4f03>
2008-06-09 06:56:43 +0000

    Add an 'AND dbid <> 0' clause...
    git-svn-id: http://cqpython.googlecode.com/svn/trunk@20 f595771e-6544-0410-99e0-6d4d7ded4f03

--- src/clearquest/sql/merge.selectStatelessEntityCounts.SQLServer.sql
+++ src/clearquest/sql/merge.selectStatelessEntityCounts.SQLServer.sql
@@ -4,6 +4,7 @@ FROM
     %(dstPrefix)s.merge_aux_map m1
 WHERE
     m1.entitydef_id = %(entityDefId)d AND
+    m1.%(dbDbIdColumn)s <> 0 AND
     m1.%(dbDbIdColumn)s IS NOT NULL AND (
         %(otherDbIdColumns)s
     )

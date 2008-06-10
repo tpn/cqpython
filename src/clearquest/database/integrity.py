0b0d52776cd68bd486fadfe8e89cc3aff575a1b2
trent.p.nelson <trent.p.nelson@f595771e-6544-0410-99e0-6d4d7ded4f03>
2008-06-10 06:44:51 +0000

    Add DatabaseIntegrity class and checkDatabase()/fixDatabase() helpers.  Currently, only the integrity of entity references is dealt with.
    git-svn-id: http://cqpython.googlecode.com/svn/trunk@24 f595771e-6544-0410-99e0-6d4d7ded4f03

--- src/clearquest/database/integrity.py
+++ src/clearquest/database/integrity.py
@@ -8,10 +8,15 @@ integrity of ClearQuest databases.
 
 import os
 import sys
+import itertools
 
+from itertools import chain, repeat
+
+from clearquest.db import Connection
 from clearquest.api import EntityDef, Session
+from clearquest.util import renderTextTable
 from clearquest.callback import Callback, ConsoleCallback
-from clearquest.constants import FieldType
+from clearquest.constants import CQConstant, FieldType, EntityType
 
 #===============================================================================
 # Globals
@@ -19,6 +24,14 @@ from clearquest.constants import FieldType
 __rcsid__ = '$Id$'
 __rcsurl__ = '$URL$'
 __copyright__ = 'Copyright 2008 OnResolve Ltd'
+
+#===============================================================================
+# Constants
+#===============================================================================
+class _IntegrityScope(CQConstant):
+    Fix    = 1
+    Check  = 2
+IntegrityScope = _IntegrityScope()
 
 #===============================================================================
 # Methods
@@ -36,7 +49,7 @@ def _checkRatlReplicas(session, entityDefName):
 def _checkParentChildLinks(session):
     pass
 
-def _checkEntity(session, entityDefName):
+def __processEntity(session, entityDefName):
     """
     Checks that every dbid value is valid.
     """
@@ -86,20 +99,134 @@ def _checkEntity(session, entityDefName):
             'from'   : '%s.%s t1' % (tablePrefix, entityDefDbName),
             'where'  : ' OR\n    '.join([ w % locals() for w in where ])
         }
-        
-__entityDefNameToIntegrityCheckMethod = {
-    'history'       : _checkHistory,
-    'ratl_replicas' : _checkRatlReplicas,
-    'users'         : _checkUsersOrGroups,
-    'groups'        : _checkUsersOrGroups,
-}
 
-def checkEntity(session, entityDefName):
+class DatabaseIntegrity(object):
+    def __init__(self, session, scope, output=sys.stdout):
+        self.session = session
+        self.scope = scope
+        self.output = output
+        self.valid = False
+        self.db = Connection(session)
+        self.tablePrefix = session.getTablePrefix()
+        
+        self._processEntities()
+        
+    def _processEntities(self):
+        
+        self.entities = dict()
+        
+        tablePrefix = self.session.getTablePrefix()
+        select = 'SELECT %(select)s FROM %(from)s WHERE %(where)s'
+        update = 'UPDATE %(from)s SET %(update)s WHERE %(where)s'
+        wheres = [
+            '%(fieldDbName)s IS NULL',
+            '%(fieldDbName)s NOT IN ' \
+            '(SELECT DISTINCT dbid FROM %(tablePrefix)s.%(refEntityDefDbName)s)'
+        ]
+        
+        valid = True
+        
+        for entityDef in self.session.getAllEntityDefs():
+            entityDefName = entityDef.GetName()
+            
+            if entityDefName in ('history', 'ratl_replicas'):
+                continue
+            
+            entityDefDbName = entityDef.GetDbName()
+            entityDefTable = '%s.%s' % (self.tablePrefix, entityDefDbName)
+            uniqueKey = entityDef.getUniqueKey()
+            ourInfo = uniqueKey._info()
+            
+            for fieldName in entityDef.GetFieldDefNames():
+                fieldType = entityDef.GetFieldDefType(fieldName)
+                fieldDbName = entityDef.getFieldDbName(fieldName)
+                
+                if fieldType != FieldType.Reference:
+                    continue
+                
+                refEntityDef = entityDef.GetFieldReferenceEntityDef(fieldName)
+                refEntityDefDbName = refEntityDef.GetDbName() 
+                refUniqueKey = refEntityDef.getUniqueKey()
+                refInfo = refUniqueKey._info()
+                
+                nullSql = select % {
+                    'select': 'COUNT(*)',
+                    'from'  : entityDefTable,
+                    'where' : wheres[0] % locals()
+                }
+                invalidSql = select % {
+                    'select': 'COUNT(*)',
+                    'from'  : entityDefTable,
+                    'where' : wheres[1] % locals()
+                }
+                nullCount = self.db.selectSingle(nullSql)
+                invalidCount = self.db.selectSingle(invalidSql)
+                
+                assert nullCount >= 0 and invalidCount >= 0
+                
+                if nullCount > 0 or invalidCount > 0:
+                    results = {
+                        'null'    : nullCount,
+                        'invalid' : invalidCount,
+                    }
+                    # Note that we delay entering anything into the entities map
+                    # until we reach this point, when we can be certain we've
+                    # found a discrepancy.
+                    self.entities.setdefault(entityDefName, dict()) \
+                                 .setdefault(fieldName, dict())     \
+                                 .update(results)
+                
+                    if self.scope == IntegrityScope.Fix:
+                        where = [ w % locals() for w in wheres ]
+                        k = {
+                            'from'   : entityDefTable,
+                            'update' : '%s = 0' % fieldDbName,
+                            'where'  : ' OR '.join(where)
+                        }
+                        sql = update % k
+                        self.db.execute(sql)
+                    else:
+                        valid = False
+                        
+        self.valid = valid
+        if self.entities:
+            
+            fixed = 'Yes' if self.scope == IntegrityScope.Fix else 'No'
+            
+            header = ('Entity', 'Field', 'Null', 'Invalid', 'Fixed?')
+            cols = len(header)
+            rows = [ 
+                header,
+                ('',) * cols,
+            ]
+            
+            for (entity, fields) in self.entities.items():
+                count = itertools.count(0)
+                for (field, r) in fields.items():
+                    # Only print out the entity name for the first row.
+                    name = '' if count.next() else entity
+                    rows.append((name, field, r['null'], r['invalid'], fixed))
+                    
+            header = (
+                '[%s Database]' % self.session._databaseName,
+                'Entity Integrity Report'
+            )
+            renderTextTable(header, rows, output=self.output)
+            
+    def _processHistory(self):
+        pass
     
-    from twisted.internet import threads, defer, reactor
+    def _processUsersAndGroups(self):
+        pass
     
+    def _processParentChildLinks(self):
+        pass
     
+        
+def checkDatabase(session, output=sys.stdout):
+    return DatabaseIntegrity(session, IntegrityScope.Check, output=output)
+
+def fixDatabase(session, output=sys.stdout):
+    return DatabaseIntegrity(session, IntegrityScope.Fix, output=output)
+
     
-    
-    pass
- 
